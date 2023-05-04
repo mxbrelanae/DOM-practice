77d2898397d864d59ae3973c8b14e6ab1d5f5775
Bre Dale <bredale122@gmail.com>
2023-05-04 09:15:57 -0400

    Learning how to use Dom and .js together

---
+++ random-colors.js
@@ -0,0 +1,22 @@
+ function randomRGB(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+return `rgb(${r},${g},${b})` //gives the appearance of regular ... like rgb(255,255,255)
+}
+
+const letters = document.querySelectorAll('.letter');
+
+ setInterval(function(){for(let letter of letters){
+    letter.style.color= randomRGB();
+  }
+    }, 1000);
+ 
+
+
+ 
+
+
+
+
